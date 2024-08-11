ea4e6dca9fdf87ce3852ad2a831a77bc5b0e56ef
Mark Tearle <mark.tearle@qcif.edu.au>
2024-08-11 17:10:50 +0800

    Code reformatted using black

--- ckanext/s3filestore/action.py
+++ ckanext/s3filestore/action.py
@@ -28,8 +28,9 @@ except ImportError:
     # CKAN 2.6 and earlier
     from pylons import config
 
-_default_404_message = 'Resource data not found'
-_default_403_message = 'Unauthorized to read resource'
+_default_404_message = "Resource data not found"
+_default_403_message = "Unauthorized to read resource"
+
 
 @validate(schema.s3filestore_download_window)
 @side_effect_free
@@ -149,7 +150,9 @@ def download_window(context, data_dict):
 
 def _get_authorised_resource(context, data_dict):
     try:
-        return get_action('resource_show')(context, {'id': data_dict.get("resource_id", "")})
+        return get_action("resource_show")(
+            context, {"id": data_dict.get("resource_id", "")}
+        )
     except NotFound:
         raise NotFound(_default_404_message)
     except NotAuthorized:
@@ -161,26 +164,28 @@ def _get_authorised_resource(context, data_dict):
 
 def _get_s3_details(rsc):
     upload = uploader.get_resource_uploader(rsc)
-    bucket_name = config.get('ckanext.s3filestore.aws_bucket_name')
-    host_name = config.get('ckanext.s3filestore.host_name')
+    bucket_name = config.get("ckanext.s3filestore.aws_bucket_name")
+    host_name = config.get("ckanext.s3filestore.host_name")
     bucket = upload.get_strict_s3_bucket(bucket_name)
-    filename = os.path.basename(rsc['url'])
-    key_path = upload.get_path(rsc['id'], filename)
+    filename = os.path.basename(rsc["url"])
+    key_path = upload.get_path(rsc["id"], filename)
     key = filename
     if key is None:
-        log.warn("Key '{0}' not found in bucket '{1}'"
-                 .format(key_path, bucket_name))
+        log.warn("Key '{0}' not found in bucket '{1}'".format(key_path, bucket_name))
     return bucket, host_name, key_path, upload, filename
 
 
 def _sign_and_return_s3_get(bucket, host_name, key_path, upload):
-    expiry_in_seconds = config.get('ckanext.s3filestore.aws_limited_s3_expiry_in_seconds', 60)
+    expiry_in_seconds = config.get(
+        "ckanext.s3filestore.aws_limited_s3_expiry_in_seconds", 60
+    )
     s3 = upload.get_limited_s3_session()
 
     log.debug("key path is: {0}".format(key_path))
-    client = s3.client(service_name='s3', endpoint_url=host_name)
-    url = client.generate_presigned_url(ClientMethod='get_object',
-                                        Params={'Bucket': bucket.name,
-                                                'Key': key_path},
-                                        ExpiresIn=expiry_in_seconds)
+    client = s3.client(service_name="s3", endpoint_url=host_name)
+    url = client.generate_presigned_url(
+        ClientMethod="get_object",
+        Params={"Bucket": bucket.name, "Key": key_path},
+        ExpiresIn=expiry_in_seconds,
+    )
     return url, expiry_in_seconds
