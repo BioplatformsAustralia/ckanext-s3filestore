b100242b1f79de40e01d03a63afb6c595096424a
Mark Tearle <mark.tearle@qcif.edu.au>
2024-08-12 10:08:43 +0800

    Fix MANIFEST.in to include new validator code
    See: https://trello.com/c/XQgEXpzv/1968-fix-download-window-links-for-shared-resources

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.0.0+bioplatforms13',
+    version='1.0.0+bioplatforms14',
 
     description='''Host resource files using Amazon Web Service's S3 storage''',
     long_description=long_description,
